f182a6f2d8f4f21122a988455ef2422b2e4e22dc
Dennis Addo <dennis_addo@aol.com>
2017-10-28 19:45:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+
+/**
+todo: A function called shout, takes one argument and
+      returns it with all caps
+*/
+
+function shout(str) {
+  return str.toUpperCase;
+}
+
+
+/**
+todo: A function called whisper, takes one argument and
+      returns it with all lowercase
+*/
+
+function whisper(str) {
+  return str.toLowerCase;
+}
